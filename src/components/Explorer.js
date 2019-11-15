34a8de1a2abbf88548785a06e0682f0ca2a09b71
Zachary Williams <zlw11063@gmail.com>
2019-11-15 08:45:04 -0700

    Update styling for explorer

--- src/components/Explorer.js
+++ src/components/Explorer.js
@@ -6,84 +6,121 @@ import { css, jsx } from '@emotion/core'
 
 import FolderIcon from '../svgs/FolderIcon'
 import FileIcon from '../svgs/FileIcon'
-import FolderOpenIcon from '../svgs/FolderOpenIcon'
+import ArrowIcon from '../svgs/ArrowIcon'
 import CodeIcon from '../svgs/CodeIcon'
 import { SelectedContext } from './SelectedProvider'
+import { StorageContext } from './StorageContext'
 
-const styles = {
-  file: css`
-    cursor: pointer;
-    user-select: none;
-    padding-left: 15px;
-    border-left: 1px solid grey;
-    padding: 2px;
+const explorerCss = css`
+  letter-spacing: 0.5px;
+  font-size: 14px;
+`
 
-    &[data-selected='true'] {
-      background-color: var(--color-text-selected);
-      color: var(--color-background);
-      font-weight: bold;
-    }
+const itemContainerCss = css`
+  display: flex;
+  align-items: center;
+`
 
-    &:hover {
-      background-color: var(--color-background-highlight);
-    }
+const iconContainerCss = css`
+  ${itemContainerCss}
+  display: inline-block;
+  margin-right: 8px;
+`
 
-    & svg {
-      margin-right: 5px;
-      margin-left: 5px;
-      width: 16px;
-      height: 16px;
-      fill: var(--color-text-accent);
-      vertical-align: bottom;
-    }
-  `,
-  folder: css``,
-  folderContents: css`
-    padding-left: 15px;
-    border-left: 1px solid grey;
-  `,
-  folderName: css`
+const componentCss = css`
+  ${itemContainerCss}
+  cursor: pointer;
+  user-select: none;
+  padding: 2px;
+  margin-left: 20px;
+  color: white;
+  border-radius: 2px;
+
+  &:hover {
+    background-color: var(--color-background-highlight);
+  }
+
+  &[data-selected='true'] {
+    background-color: var(--color-text-selected);
     color: white;
-    padding: 2px;
-    cursor: pointer;
-    user-select: none;
-    & svg {
-      margin-right: 5px;
-      margin-left: -2px;
-      width: 16px;
-      height: 16px;
-      fill: var(--color-text-selected);
-      vertical-align: bottom;
-    }
-    &:hover {
-      background-color: var(--color-background-highlight);
-    }
-  `,
+    font-weight: bold;
+  }
+
+  & svg {
+    width: 16px;
+    height: 16px;
+    fill: var(--color-text-accent);
+    vertical-align: bottom;
+  }
+`
+
+const folderContentsCss = css`
+  padding-left: 16px;
+  border-left: 1px solid var(--color-background-highlight);
+`
+
+const folderNameCss = css`
+  ${itemContainerCss}
+  color: white;
+  padding: 4px 0;
+  cursor: pointer;
+  user-select: none;
+
+  & svg {
+    width: 16px;
+    height: 16px;
+    vertical-align: bottom;
+  }
+
+  &[is-file] svg {
+    fill: var(--color-text);
+  }
+
+  &:hover {
+    background-color: var(--color-background-highlight);
+  }
+`
+
+function ItemIcons({ Icon, color = 'var(--color-text)', isOpen, hideArrow }) {
+  return (
+    <div css={iconContainerCss}>
+      {!hideArrow && <ArrowIcon fill="var(--color-text)" direction={isOpen ? 'down' : 'right'} />}
+      <Icon fill={color} />
+    </div>
+  )
 }
 
-function ExportedComponent ({selected, onClick, displayName}) {
+function ExportedComponent({ selected, onClick, displayName }) {
   return (
-    <div
-      css={styles.file}
-      data-selected={selected}
-      onClick={onClick}
-    >
-      <CodeIcon />
+    <div css={componentCss} data-selected={selected} onClick={onClick}>
+      <ItemIcons hideArrow Icon={CodeIcon} />
       {displayName}
     </div>
   )
 }
 
-function FolderFile ({isFile, path, tree, SelectedComponent, updateSelectedComponent}) {
-  const [isOpen, setOpen] = React.useState(true)
+function FolderFile({ isFile, path, tree, SelectedComponent, updateSelectedComponent }) {
+  const { getItem, setItem } = useContext(StorageContext)
+  const storageKey = `DRAFT_${path}`
+  const isOpen = getItem(storageKey, false)
   return (
-    <div css={styles.folder}>
-      <div css={styles.folderName} onClick={() => setOpen(o => !o)}>
-        {isFile ? <FileIcon fill="#fff" /> : isOpen ? <FolderOpenIcon /> : <FolderIcon />}
+    <div>
+      <div
+        is-file={isFile ? '' : undefined}
+        css={folderNameCss}
+        onClick={() => setItem(storageKey, !isOpen)}
+      >
+        {/* eslint-disable-next-line no-nested-ternary */}
+        <ItemIcons
+          Icon={isFile ? FileIcon : FolderIcon}
+          isOpen={isOpen}
+          color={isFile ? 'var(--color-text)' : 'var(--color-text-selected)'}
+        />
         {path}
       </div>
+
       {isOpen && (
-        <div css={styles.folderContents}>
+        <div css={folderContentsCss}>
           <RecursiveFileSystem
             tree={tree}
             displayComponents={isFile}
@@ -112,7 +149,7 @@ const RecursiveFileSystem = ({
       const [filePath, components] = value
       components.forEach(component => {
         children.push(
-          <ExportedComponent 
+          <ExportedComponent
             displayName={component.displayName}
             selected={
               SelectedComponent.meta.filePath === filePath &&
@@ -143,7 +180,7 @@ export default function Explorer({ componentTree }) {
   const { SelectedComponent, updateSelectedComponent } = useContext(SelectedContext)
 
   return (
-    <div>
+    <div css={explorerCss}>
       <RecursiveFileSystem
         tree={componentTree}
         SelectedComponent={SelectedComponent}
---
+++ src/svgs/ArrowIcon.js
@@ -0,0 +1,31 @@
+import React from 'react'
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core'
+
+const arrowCss = css`
+  &[data-direction='up'] {
+    transform: rotate(180deg);
+  }
+  &[data-direction='left'] {
+    transform: rotate(90deg);
+  }
+  &[data-direction='right'] {
+    transform: rotate(270deg);
+  }
+`
+
+export default function ArrowIcon({ direction, ...props }) {
+  return (
+    <svg
+      data-direction={direction}
+      css={arrowCss}
+      focusable="false"
+      viewBox="0 0 24 24"
+      aria-hidden="true"
+      role="presentation"
+      {...props}
+    >
+      <path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z" />
+    </svg>
+  )
+}
