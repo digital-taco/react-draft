c6c8e523e934347e5c8516d0d14ade063df75b05
Kyle West <kyle@familysearch.org>
2020-09-17 07:24:33 -0600

    [test] fix tests tha have been broken for a while

--- test/react-draft.test.js
+++ test/react-draft.test.js
@@ -1,22 +1,27 @@
 const { cleanup, renderPage } = require('./setup')
 
-afterAll(cleanup);
+afterAll(cleanup)
 
 test(`Should be able to switch between Activities`, () => {
-  return renderPage().then(async ({ browser, page }) => {
-    await page.waitFor('[data-test-explorer-icon]');
+  return renderPage().then(async ({ page }) => {
+    await page.waitFor('[data-test-explorer-icon]')
 
     async function checkActivities() {
-      return await page.evaluate(() => {
-        let explorer = document.querySelector('[data-test-explorer-icon]').dataset.selected === ''
-        let props = document.querySelector('[data-test-props-icon]').dataset.selected === ''
-        let settings = document.querySelector('[data-test-settings-icon]').dataset.selected === ''
-        return {explorer, props, settings}
+      return page.evaluate(() => {
+        const explorer =
+          document.querySelector('[data-test-explorer-icon]').parentElement.dataset.selected === ''
+        const props =
+          document.querySelector('[data-test-props-icon]').parentElement.dataset.selected === ''
+        const code =
+          document.querySelector('[data-test-code-icon]').parentElement.dataset.selected === ''
+        const settings =
+          document.querySelector('[data-test-settings-icon]').parentElement.dataset.selected === ''
+        return { explorer, props, code, settings }
       })
     }
 
     // Explorer should already be selected
-    let testExplorer = await checkActivities()
-    expect(testExplorer).toEqual({explorer: true, props: false, settings: false})
+    const testExplorer = await checkActivities()
+    expect(testExplorer).toEqual({ explorer: true, props: false, code: false, settings: false })
   })
 }, 30000) // first tests sometimes take a while while the page loads
